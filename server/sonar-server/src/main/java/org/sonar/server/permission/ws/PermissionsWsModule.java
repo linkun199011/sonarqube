620de497502a0d22592fd155b797023171f3b839
Daniel Schwarz <daniel.schwarz@sonarsource.com>
2017-11-25 15:27:21 +0100

    Merged SearchProjectPermissionsDataLoader into Action class

--- server/sonar-server/src/main/java/org/sonar/server/permission/ws/PermissionsWsModule.java
+++ server/sonar-server/src/main/java/org/sonar/server/permission/ws/PermissionsWsModule.java
@@ -67,7 +67,6 @@ public class PermissionsWsModule extends Module {
       TemplateGroupsAction.class,
       BulkApplyTemplateAction.class,
       // utility classes
-      SearchProjectPermissionsDataLoader.class,
       SearchTemplatesDataLoader.class,
       PermissionWsSupport.class);
   }
